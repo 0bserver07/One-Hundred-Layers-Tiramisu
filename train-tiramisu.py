aec13e598d73a00ab3a50e6c18d6f00cf6dbf619
tmos <yad.faeq@gmail.com>
2017-03-31 00:26:59 -0700

    testing the 103 fc-densenet

--- train-tiramisu.py
+++ train-tiramisu.py
@@ -3,24 +3,35 @@ from __future__ import print_function
 import os
 
 
-
-
 import keras.models as models
-from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Merge, Permute
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, Deconvolution2D, Cropping2D
+from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
+from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, Cropping2D
 from keras.layers.normalization import BatchNormalization
-from keras.callbacks import ModelCheckpoint
+
+from keras.layers import Conv2D, Conv2DTranspose
+
 from keras.optimizers import RMSprop
-from keras import backend as K
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Input, merge
 from keras.regularizers import l2
-from keras.layers.pooling import AveragePooling2D
 from keras.models import Model
+
+
+
+from keras import backend as K
 
 import cv2
 import numpy as np
 import json
-np.random.seed(07) # 0bserver07 for reproducibility
+
+K.set_image_dim_ordering('th')
+
+import cv2
+import numpy as np
+import json
+
+
+np.random.seed(7) # 0bserver07 for reproducibility
 
 
 
@@ -64,12 +75,12 @@ test_data = np.load('./data/test_data.npy')
 test_label = np.load('./data/test_label.npy')
 
 # load the model:
-# with open('tiramisu_fc_dense103_model.json') as model_file:
-#     tiramisu = models.model_from_json(model_file.read())
+with open('tiramisu_fc_dense103_model.json') as model_file:
+    tiramisu = models.model_from_json(model_file.read())
 
 
 nb_layers = [4, 5, 7, 10, 12, 15]
-tiramisu = create_fc_dense_net(nb_classes=12,img_dim=(3, 224, 224), nb_layers=nb_layers)
+# tiramisu = create_fc_dense_net(nb_classes=12,img_dim=(3, 224, 224), nb_layers=nb_layers)
 
 # section 4.1 from the paper
 optimizer = RMSprop(lr=1e-03, rho=0.9, epsilon=1e-08, decay=0.995)
@@ -82,14 +93,14 @@ checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1,
 callbacks_list = [checkpoint]
 
 nb_epoch = 100
-batch_size = 3
+batch_size = 1
 
 #
-tiramisu.load_weights('weights/tiramisu_model_weight_40.hdf5')
+# tiramisu.load_weights('weights/tiramisu_model_weight_40.hdf5')
 #
 
 # Fit the model
-history = tiramisu.fit(train_data, train_label, callbacks=callbacks_list, batch_size=batch_size, nb_epoch=nb_epoch,
+history = tiramisu.fit(train_data, train_label, callbacks=callbacks_list, batch_size=batch_size, epochs=nb_epoch,
                     verbose=1, class_weight=class_weighting , validation_data=(test_data, test_label), shuffle=True) # validation_split=0.33
 
 # This save the trained model weights to this file with number of epochs
